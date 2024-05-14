4b890f4729bbc3d6d05ead243c573112b04de252
nisaooolle <nisaoolle@gmail.com>
2024-05-14 13:59:48 +0700

    auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import Login from "./views/pages/auth/Login";
 import Register from "./views/pages/auth/Register";
+import ForgotPass from "./views/pages/auth/ForgotPass";
 
 // END ADMIN MENU REGULASI
 
@@ -12,6 +13,7 @@ function App() {
           {/* auth */}
           <Route path="/" component={Login} exact />
           <Route path="/register" component={Register} exact />
+          <Route path="/forgotpass" component={ForgotPass} exact />
         </Switch>
       </main>
     </BrowserRouter>
--- src/utils/base_URL.js
+++
@@ -1,2 +0,0 @@
-//  export const API_DUMMY = "http://localhost:4044";
-export const API_DUMMY = "https://api-bawaslu.excellentsistem.com";
