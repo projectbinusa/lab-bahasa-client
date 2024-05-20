d5be14ade374e54e673ead0e053de89822aac2c3
nisaooolle <nisaoolle@gmail.com>
2024-05-20 16:16:25 +0700

    perbaikan loginregister & chat

--- src/views/pages/auth/Login.js
+++ src/views/pages/auth/Login.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaEye, FaEyeSlash } from 'react-icons/fa'; // Import ikon dari react-icons
+
 function Login() {
+  const [showPassword, setShowPassword] = useState(false); // State untuk mengontrol apakah password ditampilkan atau tidak
+
   return (
     <body class="font-mono ">
-      {/* <!-- Container --> */}
+      {/* Container */}
       <div class="container mx-auto ">
         <br />
         <br />
         <div class="flex justify-center px-6 my-16">
-          {/* <!-- Row --> */}
+          {/* Row */}
           <div class="w-full xl:w-3/4 lg:w-11/12 flex">
-            {/* <!-- Col --> */}
+            {/* Col */}
             <div
               class="w-full h-auto hidden lg:block lg:w-4/5 bg-cover rounded-l-lg"
               style={{
@@ -17,7 +21,7 @@ function Login() {
                   " url('https://img.freepik.com/free-vector/profile-interface-concept-illustration_114360-3360.jpg?t=st=1715662478~exp=1715666078~hmac=d78ead5c54d0721c4f7c007da66f71aef3ff0138faaa3cc16f9042adde7faa5a&w=740')",
               }}
             ></div>
-            {/* <!-- Col --> */}
+            {/* Col */}
             <div class="w-full lg:w-9/12 bg-white p-8 rounded-lg lg:rounded-l-none shadow-lg shadow-slate-400">
               <h3 class="pt-4 text-2xl text-center">Login!</h3>
               <form class="px-8 pt-6 pb-8 mb-4 bg-white rounded">
@@ -35,7 +39,7 @@ function Login() {
                     placeholder="Email"
                   />
                 </div>
-                <div class="mb-4 md:mr-2 md:mb-0">
+                <div class="mb-4 md:mr-2 md:mb-0 relative">
                   <label
                     class="block mb-2 text-sm font-bold text-gray-700"
                     for="password"
@@ -43,22 +47,24 @@ function Login() {
                     Password
                   </label>
                   <input
-                    class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border border-red-500 rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                    class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
                     id="password"
-                    type="password"
+                    type={showPassword ? "text" : "password"}
                     placeholder="******************"
                   />
-                  {/* <p class="text-xs italic text-red-500">
-                    Please choose a password.
-                  </p> */}
+                  <span 
+                    class="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer mt-5"
+                    onClick={() => setShowPassword(!showPassword)} // Mengubah state showPassword ketika ikon diklik
+                  >
+                    {showPassword ? <FaEye /> : <FaEyeSlash />} {/* Menampilkan ikon view atau hide password sesuai dengan state showPassword */}
+                  </span>
                 </div>
-                {/* <br /> */}
                 <div class="mb-6 text-center">
                   <button
                     class="w-full px-4 py-2 font-bold text-white bg-green-500 rounded-full hover:bg-green-700 focus:outline-none focus:shadow-outline"
                     type="button"
                   >
-                    Register Account
+                    Login Account
                   </button>
                 </div>
                 <hr class="mb-6 border-t" />
@@ -70,14 +76,6 @@ function Login() {
                     Forgot Password?
                   </a>
                 </div>
-                {/* <div class="text-center">
-                  <a
-                    class="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
-                    href="#"
-                  >
-                    Already have an account? Login!
-                  </a>
-                </div> */}
               </form>
             </div>
           </div>
