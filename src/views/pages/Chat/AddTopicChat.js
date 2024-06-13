ee7cf32482e79d8ed9a22b7afdffa1ed86b57875
ZiyanHaidar <ziyanhaidar809@gmail.com>
2024-06-13 08:24:33 +0700

    integrasi topic chat

--- src/views/pages/Chat/AddTopicChat.js
+++ src/views/pages/Chat/AddTopicChat.js
@@ -11,7 +11,7 @@ const authConfig = {
   },
 };
 
-function AddGroup() {
+function AddTopicChat() {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const class_id = localStorage.getItem("class_id");
@@ -24,7 +24,7 @@ function AddGroup() {
       description: description,
       is_removed: 0
     };
-    let url_hit = `${API_DUMMY}/api/class/${class_id}/group`;
+    let url_hit = `${API_DUMMY}/api/class/${class_id}/topic_chat`;
     try {
       const response = await axios.post(url_hit, jsonData, authConfig);
       if (response.status === 200) {
@@ -48,35 +48,21 @@ function AddGroup() {
         <div className="content-page container mx-auto p-8">
           <div className="add-class mt-12 bg-white p-8 rounded-xl shadow-xl border border-gray-300">
             <h1 className="text-xl sm:text-2xl font-bold mb-3 text-gray-800">
-              Tambah Grup
+              Tambah Topic Chat
             </h1>
             <form onSubmit={saveChange}>
               <div className="relative mb-5">
                 <label className="block mb-2 text-sm font-semibold text-gray-700">
-                  Nama Grup
+                  Nama TopicChat
                 </label>
                 <input
                   type="text"
                   id="className"
                   className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2"
-                  placeholder="Masukkan Nama Grup"
+                  placeholder="Masukkan Nama Topic"
                   required
                   value={name}
                   onChange={(e) => setName(e.target.value)}
-                />
-              </div>
-              <div className="relative mb-5">
-                <label className="block mb-2 text-sm font-semibold text-gray-700">
-                  Deskripsi
-                </label>
-                <input
-                  type="text"
-                  id="classDescription"
-                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2"
-                  placeholder="Masukkan Deskripsi"
-                  required
-                  value={description}
-                  onChange={(e) => setDescription(e.target.value)}
                 />
               </div>
               <div className="flex justify-end mt-8">
@@ -95,4 +81,4 @@ function AddGroup() {
   );
 }
 
-export default AddGroup;
+export default AddTopicChat;
