b6f5b20fc5ad586bfc5a0b513fa6a9068402ec49
ZiyanHaidar <ziyanhaidar809@gmail.com>
2024-06-14 11:11:41 +0700

    memperbaiki topic chat

--- src/views/pages/Chat/TopikChat.js
+++ src/views/pages/Chat/TopikChat.js
@@ -1,96 +1,79 @@
 import React, { useEffect, useState } from "react";
-import { API_DUMMY } from "../../../utils/api";
 import axios from "axios";
 import img from "../../../component/Asset/group.png";
+import { API_DUMMY } from "../../../utils/api";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
 
 const authConfig = {
   headers: {
     "auth-event": `jwt ${localStorage.getItem("token")}`,
-    "Content-Type": "application/json",
+    "Content-Type": "multipart/form-data",
   },
 };
 
 function TopikChat() {
   const [chatTopic, setChatTopic] = useState([]);
-  const [input, setInput] = useState("");
-  const [file, setFile] = useState(null);
+  const [content, setContent] = useState("");
+  const [gambar, setGambar] = useState(null);
   const [list, setList] = useState([]);
-  const [selectedTopicChat, setselectedTopicChat] = useState(null); // State untuk menyimpan grup yang dipilih
+  const [selectedTopic, setSelectedTopicChat] = useState(null);
   const class_id = localStorage.getItem("class_id");
-  const topic_chat_id = localStorage.getItem("topic_chat_id");
 
-
-  const sendMessage = async () => {
-    if (!selectedTopicChat) {
-      console.error("Topic chat not selected.");
+  // Fungsi untuk mengirim pesan
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    if (!selectedTopic) {
+      console.error("Group not selected.");
       return;
     }
 
-    if (input.trim() !== "" || file) {
-      const formData = new FormData();
-      formData.append("content", input);
-      if (file) {
-        formData.append("gambar", file);
-      }
-      formData.append("is_group", 1);
+    const formData = new FormData();
+    formData.append("is_group", 1);
+    if (gambar) {
+      formData.append("gambar", gambar);
+    }
+    if (content) {
+      formData.append("content", content);
+    }
 
-      // Log form data to ensure content is being appended
-      for (let pair of formData.entries()) {
-          console.log(pair[0] + ': ' + pair[1]);
+    try {
+      const response = await axios.post(
+        `${API_DUMMY}/api/chat/class/${class_id}/topic_chat/${selectedTopic.id}`,
+        formData,
+        authConfig
+      );
+      if (response.status === 200) {
+        getAllDatachatTopic(selectedTopic.id);
+        setContent("");
+        setGambar(null);
       }
-
-      try {
-        const response = await fetch(
-          `${API_DUMMY}/api/chat/class/${class_id}/topic_chat/${selectedTopicChat.id}`,
-          {
-            method: "POST",
-            body: formData,
-            headers: authConfig.headers,
-          }
-        );
-        if (response.ok) {
-          const result = await response.json();
-          setChatTopic((prevMessages) => [
-            ...prevMessages,
-            {
-              content: result.content,
-              gambar: result.gambar,
-              sender_id: result.sender_id,
-              created_date: new Date().toISOString(),
-            },
-          ]);
-        } else {
-          console.error("Error sending message:", response.statusText);
-        }
-      } catch (error) {
-        console.error("Error sending message:", error);
-      }
-
-      setInput("");
-      setFile(null);
+    } catch (error) {
+      console.error("Error sending message:", error);
     }
   };
 
-
+  // Fungsi untuk meng-handle perubahan file gambar
   const handleFileChange = (e) => {
     const selectedFile = e.target.files[0];
-    setFile(selectedFile);
+    setGambar(selectedFile);
   };
 
+  // Fungsi untuk memformat tanggal
   const formatDate = (timestamp) => {
     const date = new Date(timestamp);
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const day = String(date.getDate()).padStart(2, "0");
-    const hours = String(date.getHours()).padStart(2, "0");
-    const minutes = String(date.getMinutes()).padStart(2, "0");
-    const seconds = String(date.getSeconds()).padStart(2, "0");
-    return `${year}-${month}-${day} , ${hours}:${minutes}:${seconds}`;
+    return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(
+      2,
+      "0"
+    )}-${String(date.getDate()).padStart(2, "0")} , ${String(
+      date.getHours()
+    ).padStart(2, "0")}:${String(date.getMinutes()).padStart(2, "0")}:${String(
+      date.getSeconds()
+    ).padStart(2, "0")}`;
   };
 
+  // Memuat semua data grup
   const getAllData = async () => {
     try {
       const response = await axios.get(
@@ -103,14 +86,16 @@ function TopikChat() {
     }
   };
 
-  const getAllDataChatTopic = async () => {
+  // Memuat semua pesan dari sebuah grup
+  const getAllDatachatTopic = async (topic_chat_id) => {
     try {
       const response = await axios.get(
         `${API_DUMMY}/api/chat/class/${class_id}/topic_chat/${topic_chat_id}`,
         authConfig
       );
-      setChatTopic(response.data.data);
-      console.log(response.data.data);
+      // Balik urutan data dari terbaru ke terlama
+      const reversedMessages = response.data.data.slice().reverse();
+      setChatTopic(reversedMessages);
     } catch (error) {
       console.log(error);
     }
@@ -118,34 +103,33 @@ function TopikChat() {
 
   useEffect(() => {
     getAllData();
-    // getAllDataChatTopic()
   }, []);
 
   useEffect(() => {
-    if (selectedTopicChat) {
-      getAllDataChatTopic(selectedTopicChat.id);
+    if (selectedTopic) {
+      getAllDatachatTopic(selectedTopic.id);
     }
-  }, [selectedTopicChat]);
+  }, [selectedTopic]);
 
   return (
     <>
       <div className="flex justify-center gap-4">
         <div className="bg-white w-96">
-        <button className="bg-green-500 w-full h-12 rounded-t-lg">
-          <Link
-            to="/add-topic-chat"
-            className="flex items-center justify-center h-full text-white text-lg"
-          >
-            <FontAwesomeIcon icon={faPlus} className="mr-2" />
-            Add Topic Chat
-          </Link>
-        </button>
+          <button className="bg-green-500 w-full h-12 rounded-t-lg">
+            <Link
+              to="/add-topic-chat"
+              className="flex items-center justify-center h-full text-white text-lg"
+            >
+              <FontAwesomeIcon icon={faPlus} className="mr-2" />
+              Tambah Topik Chat
+            </Link>
+          </button>
           <div className="p-2">
             {list.map((data, index) => (
               <div
                 key={index}
                 className="bg-green-300 rounded-lg p-2 flex gap-4 cursor-pointer"
-                onClick={() => setselectedTopicChat(data)}
+                onClick={() => setSelectedTopicChat(data)}
               >
                 <div className="border-2 w-fit rounded-full border-green-500">
                   <img className="w-9" src={img} alt="" />
@@ -159,32 +143,41 @@ function TopikChat() {
           <div className="flex-1 bg-white overflow-y-hidden">
             <div className="border-2 border-green-500 bg-green-500 h-10">
               <h1 className="text-white text-lg ml-4 font-semibold">
-                {selectedTopicChat ? selectedTopicChat.name : "Pilih topic chat"}
+                {selectedTopic ? selectedTopic.name : "Pilih topik chat"}
               </h1>
             </div>
             <div className="overflow-y-scroll overflow-scroll h-[90%]">
               {chatTopic.map((message, index) => (
                 <div
-                  className={`px-4 py-2 ${
+                  key={index}
+                  className={`px-4 py-2 flex ${
                     message.sender_id == localStorage.getItem("user_id")
-                      ? "flex justify-start"
-                      : "flex justify-end"
+                      ? "justify-end"
+                      : "justify-start"
                   }`}
-                  key={index}>
+                >
                   <div className="flex w-96 items-center">
-                    <img
-                      className="w-8 h-8 rounded-full"
-                      src="https://picsum.photos/50/50"
-                      alt="User Avatar"
-                    />
-                    <div className="bg-green-500 text-white rounded-lg p-2 w-[90%] shadow ml-2">
+                    {message.sender_id != localStorage.getItem("user_id") && (
+                      <img
+                        className="w-8 h-8 rounded-full"
+                        src="https://picsum.photos/50/50"
+                        alt="User Avatar"
+                      />
+                    )}
+                    <div
+                     key={index}
+                      className={`${
+                        message.sender_id == localStorage.getItem("user_id")
+                          ? "bg-blue-500 text-white"
+                          : "bg-green-500 text-white"
+                      } rounded-lg p-2 w-[90%] shadow ml-2`}
+                    >
                       <div className="flex justify-between">
                         <p>{message.content}</p>
                         <button className="">
-                          <i class="fa-solid fa-ellipsis-vertical"></i>
+                          <i className="fa-solid fa-ellipsis-vertical"></i>
                         </button>
                       </div>
-                      <p>{formatDate(message.created_date)}</p>
                       {message.gambar && (
                         <img
                           className="mt-2"
@@ -193,33 +186,37 @@ function TopikChat() {
                           style={{ maxWidth: "200px", maxHeight: "200px" }}
                         />
                       )}
+                      <p>{formatDate(message.created_date)}</p>
                     </div>
+                    {message.sender_id == localStorage.getItem("user_id") && (
+                      <img
+                        className="w-8 h-8 rounded-full"
+                        src="https://picsum.photos/50/50"
+                        alt="User Avatar"
+                      />
+                    )}
                   </div>
                 </div>
               ))}
             </div>
           </div>
           <div className="bg-gray-100 px-4 py-2">
-            <div className="flex items-center">
+            <form onSubmit={sendMessage} className="flex items-center">
               <input
                 className="w-full border rounded-full py-2 px-4 mr-2"
                 type="text"
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
                 placeholder="Ketik pesan anda..."
               />
               <input type="file" onChange={handleFileChange} />
               <button
                 className="bg-green-500 hover:bg-green-700 text-white font-medium py-2 px-4 rounded-full"
-                onClick={sendMessage}>
+                type="submit"
+              >
                 Kirim
               </button>
-              {/* {!selectedTopicChat && (
-                <p className="text-red-500">
-                  Pilih grup sebelum mengirim pesan.
-                </p>
-              )} */}
-            </div>
+            </form>
           </div>
         </div>
       </div>
